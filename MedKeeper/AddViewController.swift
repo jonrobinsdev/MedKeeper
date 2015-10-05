d51f57bcfb501d9fdc043946b7a319c6a6bf7b44
Jonathan Robins <jonrobns@gmail.com>
2015-10-05 00:07:01 -0400

    initial commit

--- MedKeeper/AddViewController.swift
+++ MedKeeper/AddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  AddViewController.swift
 //  MedKeeper
 //
 //  Created by Jonathan Robins on 10/4/15.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class AddViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
