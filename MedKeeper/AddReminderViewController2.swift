99d6da91546fc407b7f366c389d0e1357e224f1d
Jonathan Robins <jonrobns@gmail.com>
2015-10-06 17:30:25 -0400

    finishing up picker view for medication dosage

--- MedKeeper/AddReminderViewController2.swift
+++ MedKeeper/AddReminderViewController2.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class AddReminderViewController2: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     @IBOutlet var pickerView: UIPickerView!
-    var pickerViewData = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], ["herp", "derp", "flerp"]];
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
+    var pillPickerViewData = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], ["pills", "mg"]];
+    var syrupPickerViewData = [["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"], ["tsp", "Tbsp", "mL"]];
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +27,35 @@ class AddReminderViewController2: UIViewController, UIPickerViewDelegate, UIPick
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return pickerViewData.count
+        if(segmentedControl.selectedSegmentIndex == 0){
+            return pillPickerViewData.count
+        }
+        else{
+            return syrupPickerViewData.count
+        }
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-       return pickerViewData[component].count
+        if(segmentedControl.selectedSegmentIndex == 0){
+            return pillPickerViewData[component].count
+        }
+        else{
+            return syrupPickerViewData[component].count
+        }
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerViewData[component][row]
+        if(segmentedControl.selectedSegmentIndex == 0){
+            return pillPickerViewData[component][row]
+        }
+        else{
+            return syrupPickerViewData[component][row]
+        }
+    }
+    @IBAction func segmentControlChanges(sender: AnyObject) {
+        self.pickerView.reloadAllComponents();
+        pickerView.selectRow(0, inComponent: 0, animated: true);
+        pickerView.selectRow(0, inComponent: 1, animated: true);
     }
     
     /*
