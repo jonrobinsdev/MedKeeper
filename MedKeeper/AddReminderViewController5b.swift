4da3983bde855d512159e5a9c6db6a0bbc250148
Jonathan Robins <jonrobns@gmail.com>
2015-10-11 22:55:06 -0400

    working on interval alarm clock layout

--- MedKeeper/AddReminderViewController5b.swift
+++ MedKeeper/AddReminderViewController5b.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class AddReminderViewController5b: UIViewController {
+class AddReminderViewController5b: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet var fromDatePicker: UIDatePicker!
+    @IBOutlet var toDatePicker: UIDatePicker!
+    @IBOutlet var intervalPickerView: UIPickerView!
+    var pickerViewData = ["1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5", "13", "13.5", "14", "14.5", "15", "15.5", "16", "16.5", "17", "17.5", "18", "18.5", "19", "19.5", "20", "20.5", "21", "21.5", "22", "22.5", "23", "23.5", "24"];
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.intervalPickerView.delegate = self
+        self.intervalPickerView.dataSource = self
+        self.intervalPickerView.selectRow(4, inComponent: 0, animated: true)
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +26,18 @@ class AddReminderViewController5b: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 47
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerViewData[row]
+    }
+
     
 
     /*
