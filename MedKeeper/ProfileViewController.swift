b90af782ffa75edc080fb48e1a14837538438162
Jonathan Robins <jonrobns@gmail.com>
2015-10-22 17:02:26 -0400

    logic for adding new profile

--- MedKeeper/ProfileViewController.swift
+++ MedKeeper/ProfileViewController.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
     @IBOutlet var profilesTableView: UITableView!
     var profileArray: NSMutableArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         let defaults = NSUserDefaults.standardUserDefaults()
         profileArray = defaults.objectForKey("ProfileArray") as! NSMutableArray
         
@@ -27,7 +26,6 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
@@ -52,7 +50,53 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
             return cell
     }
 
-
-
+    @IBAction func addProfileButtonPressed(sender: AnyObject) {
+        var tField: UITextField!
+        func configurationTextField(textField: UITextField!)
+        {
+            textField.returnKeyType = UIReturnKeyType.Done
+            tField = textField
+            tField.delegate = self
+        }
+        let alert = UIAlertController(title: "Please Enter A Name For Your New Medicine Profile", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addTextFieldWithConfigurationHandler(configurationTextField)
+        //this action is necessary for some reason or else keyboard doesn't dismiss correctly
+        alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.Default, handler:nil))
+        self.presentViewController(alert, animated: true, completion: {
+            print("New profile alert view appeared!")
+        })
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if(textField.text?.characters.count > 0){
+            saveNewProfileNameToNSUserDefaults(textField.text!)
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
+    func saveNewProfileNameToNSUserDefaults(name : NSString){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        let tempNewProfileArray : NSMutableArray = defaults.objectForKey("ProfileArray") as! NSMutableArray
+        let newProfileArray : NSMutableArray = []
+        
+        for var i = 0; i < tempNewProfileArray.count; i++ {
+            newProfileArray.insertObject(tempNewProfileArray[i], atIndex: i)
+        }
+        
+        newProfileArray.insertObject(name, atIndex: newProfileArray.count)
+        
+        defaults.setObject(newProfileArray, forKey: "ProfileArray")
+        defaults.synchronize()
+        
+        self.profileArray = newProfileArray
+        self.profilesTableView.reloadData()
+        
+        let indexPath = NSIndexPath(forRow: self.profileArray.count-1, inSection: 0)
+        self.profilesTableView.scrollToRowAtIndexPath(indexPath, atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
+    }
 }
 
